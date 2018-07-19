665c2f90a5dea496b7f213e374ac005223d08001
Armando Sosa <asosa@nolimit-studio.com>
2018-07-19 14:18:57 -0500

    split tab components into multiple files

--- components/timer-tab-nav.js
+++ components/timer-tab-nav.js
@@ -1,96 +1,6 @@
 import React from 'react';
 import Timer from './timer';
-import * as colors from '../lib/colors';
-
-const ProgressBorder = ({ selected, progress }) => (
-  <div className="outside">
-    {selected && (
-      <div
-        className="inside"
-        style={{
-          width: `${progress}%`,
-        }}
-      />
-    )}
-    <style jsx>{`
-      .outside {
-        width: 100%;
-        height: 2px;
-        background-color: ${colors.progressGray};
-        position: absolute;
-        left: 0;
-        bottom: 0;
-      }
-      .inside {
-        width: 0%;
-        height: 4px;
-        background-color: ${colors.pink};
-        position: absolute;
-        left: 0;
-        bottom: 0;
-      }
-    `}</style>
-  </div>
-);
-
-const ProgressTabItem = ({ title, subtitle, progress, selected, onClick }) => (
-  <a href={'#'} onClick={onClick} className={selected ? 'selected' : ''}>
-    <span className="title">{title}</span>
-    <span className="subtitle">{subtitle}</span>
-    <ProgressBorder selected={selected} progress={progress} />
-    <style jsx>{`
-      a {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        padding: 22px;
-        position: relative;
-        margin: 0 10px;
-        text-decoration: none;
-      }
-
-      a:first-child {
-        margin-left: 0;
-      }
-      a:last-child {
-        margin-right: 0;
-      }
-
-      span {
-        display: block;
-      }
-      .title {
-        font-weight: 400;
-        font-size: 20px;
-        color: ${colors.notBlack};
-      }
-      .subtitle {
-        font-weight: 400;
-        font-size: 14px;
-        color: ${colors.gray};
-      }
-    `}</style>
-  </a>
-);
-
-const Nav = ({ progress, tabs, changeHandler }) => (
-  <nav>
-    {tabs.map(tab => (
-      <ProgressTabItem
-        key={tab.name}
-        onClick={changeHandler(tab)}
-        progress={progress}
-        {...tab}
-      />
-    ))}
-    <style jsx>{`
-      nav {
-        display: flex;
-      }
-    `}</style>
-  </nav>
-);
+import Nav from './progress-nav';
 
 export default class TabsNav extends React.Component {
   state = {
@@ -110,7 +20,6 @@ export default class TabsNav extends React.Component {
     if (curr >= tabs.length) {
       curr = 0;
     }
-    // console.log('CURR', curr);
     return this.handleChange(tabs[curr]);
   };
 
